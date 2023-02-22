c9138f255f401f08a760c3a4fda806727dbce6b4
thirdy454 <gayares@gmail.com>
2023-02-23 00:30:27 +0800

    add nalang and documentation

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,56 @@ public class Main
     static float totalCash = 0f, income = 0f, outcome = 0f;
     static Scanner userInput;
 
-    // static String[50][3] viewHistoryIncome;
-    // static String[50][3] viewHistoryOutcome;
-    // static String[50][3] viewHistoryOnlinePayment;
+    //initiate ArrayList
+    static ArrayList<String> incomeName = new ArrayList<String>();
+    static ArrayList<Float> incomeCash = new ArrayList<Float>();
+    static ArrayList<String> outcomeName = new ArrayList<String>();
+    static ArrayList<Float> outcomeCash = new ArrayList<Float>();
 
     public static void main(String[] args) {
 
         userInput = new Scanner(System.in);
         System.out.println("Thirdy Financial Management System Mobile App");
+
+        initiateIncomeOutCome(); //Store Income Name and Income Cash
         homePage();
+
     }
+
+    private static void initiateIncomeOutCome() {
+        incomeName.add("Allowance");
+        incomeCash.add(200f);
+        incomeName.add("Net Income");
+        incomeCash.add(2000f);
+        incomeName.add("Freelance");
+        incomeCash.add(1500f);
+        incomeName.add("Bonus");
+        incomeCash.add(2300f);
+        incomeName.add("May nagbayad Utang");
+        incomeCash.add(100f);
+
+        outcomeName.add("Pamasahe");
+        outcomeCash.add(20f);
+        outcomeName.add("Jolibee");
+        outcomeCash.add(90f);
+        outcomeName.add("Red Horse");
+        outcomeCash.add(120f);
+        outcomeName.add("Wifi PLDT");
+        outcomeCash.add(120f);
+    }
+
 
     public static void homePage(){
         int chooseFeatures;
         boolean wrongInput = true;
         do {
             wrongInput = false;
-            System.out.println("Total Cash: " + totalCash);
-            System.out.println("Debt: " + income);
-            System.out.println("Borrower: " + outcome);
+            System.out.println("Current Cash: " +  (totalCash(incomeCash) - totalCash(outcomeCash)));
 
-            System.out.println("Option: ");
-            System.out.println("[1]Income");
-            System.out.println("[2]Outcome");
-            System.out.println("[3]Online Payment");
+            System.out.println("\nOption: ");
+            System.out.println("[1] Income");
+            System.out.println("[2] Outcome");
+            System.out.println("[3] Exit");
 
             System.out.println("\nType >");
             chooseFeatures = userInput.nextInt();
@@ -42,10 +68,7 @@ public class Main
                     outcome();
                     break;
                 case 3:
-                    budgeting();
-                    break;
-                case 4:
-                    onlinePayment();
+                    System.exit(0);
                     break;
                 default:
                     System.out.println("Wrong!! Try Again");
@@ -59,22 +82,21 @@ public class Main
         boolean wrongInput = true;
         do {
             wrongInput = false;
-            System.out.println("Total Income: " + income);
+            System.out.println("Total Income: " + totalCash(incomeCash));
             System.out.println("========================");
-            System.out.println("Option: ");
-            System.out.println("[1]View History");
-            System.out.println("[2]Add Income");
-            System.out.println("[3]HomePage");
+
+            viewHistory("incomes");
+
+            System.out.println("\n\nOption: ");
+            System.out.println("[1] Add Income");
+            System.out.println("[2] Back To Menu");
             System.out.println("\nType >");
             chooseFeaturesIncome = userInput.nextInt();
             switch(chooseFeaturesIncome){
                 case 1:
-                    viewHistory("incomes");
-                    break;
-                case 2:
                     add("incomes");
                     break;
-                case 3:
+                case 2:
                     homePage();
                     break;
                 default:
@@ -84,27 +106,53 @@ public class Main
         }while (wrongInput == true);
     }
 
+    public static void outcome(){
+        int chooseFeaturesOutcome;
+        boolean wrongInput = true;
+        do {
+            wrongInput = false;
+            System.out.println("Total Outcome: " + totalCash(outcomeCash));
+            System.out.println("========================");
+
+            viewHistory("outcomes");
+
+            System.out.println("\n\nOption: ");
+            System.out.println("[1] Add Income");
+            System.out.println("[2] Back To Menu");
+            System.out.println("\nType >");
+            chooseFeaturesOutcome = userInput.nextInt();
+            switch(chooseFeaturesOutcome){
+                case 1:
+                    add("outcomes");
+                    break;
+                case 2:
+                    homePage();
+                    break;
+                default:
+                    System.out.println("Wrong!! Try Again");
+                    wrongInput = true;
+            }
+        }while (wrongInput == true);
+    }
 
     public static void viewHistory(String transaction){
         if(transaction.equals("incomes")){
-
-            System.out.println("Income History");
-            System.out.println("Date     Name      Price");
-
-            ArrayList<String> cars = new ArrayList<String>();
-            cars.add("Volvo");
-            cars.add("BMW");
-            cars.add("Ford");
-            cars.add("Mazda");
-            System.out.println(cars);
-            for(int i = 0; i<cars.size(); i++){
-                //   System.out.println(viewHistoryIncomeDate  +  "   "  + viewHistoryIncomeTitle.get(i)   +   "      Php "  + viewHistoryIncomePrice.get(i) );
-                System.out.println(cars.get(i));
-            }
+            System.out.println("Income History\n");
+            printHistory(incomeName, incomeCash);
         }else if(transaction.equals("outcomes")){
-            System.out.println("outcome");
+            System.out.println("Outcome History\n");
+            printHistory(outcomeName, outcomeCash);
         }
     }
+
+    public static void printHistory(ArrayList<String> y, ArrayList<Float> x){
+        System.out.println("Name\t\t\t\t\t\tPrice");
+
+        for(int i = 0; i<y.size(); i++){
+            System.out.println(y.get(i) + "\t\t\t\t" + x.get(i));
+        }
+    }
+
 
     public static void add(String transaction){
         if(transaction.equals("income")){
@@ -114,17 +162,16 @@ public class Main
         }
     }
 
-    public static void outcome(){
-        System.out.println("Outcome");
+
+    //Compute total Income
+    public static float totalCash(ArrayList<Float> x){
+          float total = 0f;
+           for(int i = 0; i< x.size(); i++){
+               total += x.get(i);
+          }
+           return  total;
     }
 
-    public static void budgeting(){
-        System.out.println("budgeting");
-    }
-
-    public static void onlinePayment(){
-        System.out.println("Online Payment");
-    }
 
 }    
 
